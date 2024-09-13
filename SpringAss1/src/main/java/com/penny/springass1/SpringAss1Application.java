9dac0e4fc121cc66e28358b8be7fdc3ac6009073
Pingyi Xu <xu.pin@northeastern.edu>
2024-09-12 23:53:28 -0700

    Set up Spring

---
+++ SpringAss1/src/main/java/com/penny/springass1/SpringAss1Application.java
@@ -0,0 +1,13 @@
+package com.penny.springass1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAss1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAss1Application.class, args);
+    }
+
+}
---
+++ SpringAss1/src/test/java/com/penny/springass1/SpringAss1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.penny.springass1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAss1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
